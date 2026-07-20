dc9cb09d4b353504bb708551702bd595af377aac
ajaynarware2006-collab <ajaynarware2006@gmail.com>
2026-07-20 18:28:52 +0530

    feat: complete FastAPI environment setup and first working API

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
 
 app=FastAPI()
+
+@app.get("/")
+def hello():
+    details={
+        "name": "Ajay Narware",
+        "role": "Student",
+        "goal": "AI engineer",
+        "banch":"AIML"
+    }
+    return details
